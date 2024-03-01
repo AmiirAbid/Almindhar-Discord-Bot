50f30537141be4d1ecb872f6d194a36ae7086b07
AmiirAbid <amirabid401@gmail.com>
2024-03-02 00:48:59 +0100

    added new functionnalities

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
 import discord
 import responses
+from dotenv import load_dotenv
 import os
+import random
+
+# List of funny responses
+funny_responses = [
+    "Hold your horses, we're not in the fast lane just yet!",
+    "Just simmer down like a slow-cooked stew while I handle this.",
+    "Patience, young padawan. Processing your request, I am.",
+    "Sit tight and grab some popcorn. This'll be worth the wait!",
+    "Hang in there! Rome wasn't built in a day, and neither are bot responses.",
+    "Don't worry, I'm not just twiddling my virtual thumbs here. Processing in progress!",
+    "Chillax like a penguin on an iceberg. We'll get there.",
+    "Loading... Just like a cat deciding whether to grace you with its presence.",
+    "Hold on tight! I'm untangling the virtual spaghetti to get to your request.",
+    "I'm on it like a sloth on a leisurely stroll. Expect a response soon!"
+]
 
 async def send_message(message, user_message) :
     try :
         response = responses.hanle_response(user_message)
         await message.channel.send(response)
     except Exception as e :
+        print('exception')
         print(e)
 
 def run_discord_bot() :
-    TOKEN = os.environ.get('dc_token')
+    load_dotenv()
+    TOKEN = os.getenv('dc_token')
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -24,8 +42,17 @@ def run_discord_bot() :
         if message.author == client.user :
             return
 
+        username = str(message.author.mention)
         user_message = str(message.content)
 
-        await send_message(message, user_message)            
+        if '?agencies' in user_message :
+            await send_message(message, user_message)
+
+        elif '?properties' in user_message :
+            # Selecting a random response
+            random_response = random.choice(funny_responses)
+            await message.channel.send(f'{username} {random_response}')
+
+            await send_message(message, user_message)
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,23 +1,63 @@
-import agency_finder
+from website_related_functionnalities import agency_finder, property_calculator, author_finder
 
 def hanle_response (message) -> str :
 
-    if message == '?agencies' :
-        agencies = agency_finder.find_agency()
+    if message == '?agencies today' :
+        agencies = agency_finder.find_agency_today()
         n_agencies = len(agencies)
 
         if n_agencies == 0 :
-            return 'No agencies have subscribed yesterday'
+            return '@everyone no agencies have subscribed today'
 
         else :
-            return f'{n_agencies} agencies have subscribed\n{'\n'.join(agencies)}'
+            return f'@everyone {n_agencies} agencies have subscribed today\n{'\n'.join(agencies)}'
+
+    elif message == '?agencies yesterday' :
+        agencies = agency_finder.find_agency_yesterday()
+        n_agencies = len(agencies)
+
+        if n_agencies == 0 :
+            return '@everyone no agencies have subscribed yesterday'
+
+        else :
+            return f'@everyone {n_agencies} agencies have subscribed yesterday\n{'\n'.join(agencies)}'
 
     elif message[:10] == '?agencies ' :
         agencies = agency_finder.find_agency_by_date(message[10:])
         n_agencies = len(agencies)
 
         if n_agencies == 0 :
-            return f'No agencies have subscribed on {message[10:]}'
+            return f'@everyone No agencies have subscribed on {message[10:]}'
 
         else :
-            return f'{n_agencies} agencies have subscribed on {message[10:]}\n{'\n'.join(agencies)}'
+            return f'@everyone {n_agencies} agencies have subscribed on {message[10:]}\n{'\n'.join(agencies)}'
+
+    elif message == '?properties today' :
+        authors = property_calculator.find_property_today()
+        n_properties = sum(authors.values())
+
+        if n_properties == 0 :
+            return '@everyone no properties were published today'
+
+        else :
+            output = f'@everyone {n_properties} properties were published today by:'
+            for author in authors :
+                author_name = author_finder.find_author(author)
+                output += f'\n{author_name}: {str(authors[author])}'
+
+            return output
+
+    elif message == '?properties yesterday' :
+        authors = property_calculator.find_property_yesterday()
+        n_properties = sum(authors.values())
+
+        if n_properties == 0 :
+            return f'@everyone no properties were published yesterday'
+
+        else :
+            output = f'@everyone {n_properties} properties were published yesterday by:'
+            for author in authors :
+                author_name = author_finder.find_author(author)
+                output += f'\n{author_name}: {str(authors[author])}'
+
+            return output
